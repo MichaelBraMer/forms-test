bb828c10d1e8e5817c9586137d82b4c485cc3c2c
MichaelBraMer <mkbravom@gmail.com>
2024-11-14 17:07:47 -0300

    add back env to docker

--- forms-backend/src/app.module.ts
+++ forms-backend/src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FormModule } from './forms/form.module';
 import { AnswerModule } from './answers/answer.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/forms'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL') || 'mongodb://localhost:27017/forms',
+      }),
+      inject: [ConfigService],
+    }),
     FormModule,
     AnswerModule,
   ],
